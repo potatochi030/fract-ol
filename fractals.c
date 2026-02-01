5d667be27bfcdd64a3669ce62e6cab56c6e2a9cd
potatochi030 <oqdehaysha@gmail.com>
2026-02-01 16:37:00 +0300

    T is made

--- fractals.c
+++ fractals.c
@@ -15,33 +15,33 @@ int     get_color(int i, int max_i)
 	b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
     return ((r << 16) | (g << 8) | b);
 }
-int     get_iteration(double zx, double zy, t_mlx fract)
+int     get_iteration(double zx, double zy, t_mlx *fract)
 {
     double  tmp;
     int     i;
 
     i = 0;
-    while ((zx * zx) + (zy * zy) < 4 && i < fract.max_iter)
+    while ((zx * zx) + (zy * zy) < 4 && i < fract->max_iter)
     {
-        tmp  = zx * zx - zy * zy + fract.julia.re;
-        zy = 2 * zx * zy + fract.julia.im;
+        tmp  = zx * zx - zy * zy + fract->julia.re;
+        zy = 2 * zx * zy + fract->julia.im;
         zx = tmp;
         i++;
     }
     return (i);
 }
-void    put_pix(t_mlx fract, int x, int y)
+void    put_pix(t_mlx *fract, int x, int y)
 {
     double  cx;
     double  cy;
     int     i;
     
-    if (fract.type == mandelbrot)
+    if (fract->type == mandelbrot)
     {
         cx = 0;
         cy = 0;
-        fract.julia.re = map_x(x, fract);
-        fract.julia.im = map_y(y, fract);
+        fract->julia.re = map_x(x, fract);
+        fract->julia.im = map_y(y, fract);
     }
     else
     {
@@ -49,10 +49,10 @@ void    put_pix(t_mlx fract, int x, int y)
         cy = map_y(y, fract);
     }
     i = get_iteration(cx, cy, fract);
-    put_colored_pix(fract, x, y, get_color(i, fract.max_iter));
+    put_colored_pix(fract, x, y, get_color(i, fract->max_iter));
 }
 
-void    print_fractol(t_mlx mlx)
+void    print_fractol(t_mlx *mlx)
 {
     int x;
     int y;
@@ -68,5 +68,5 @@ void    print_fractol(t_mlx mlx)
         }
         y++;
     }
-    mlx_put_image_to_window(mlx.obj, mlx.win, mlx.img.image, 0, 0);
+    mlx_put_image_to_window(mlx->obj, mlx->win, mlx->img.image, 0, 0);
 }
--- fractol.c
+++ fractol.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chi <chi@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: aoqdeh <aoqdeh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 13:36:53 by aoqdeh            #+#    #+#             */
-/*   Updated: 2026/02/01 11:45:05 by chi              ###   ########.fr       */
+/*   Updated: 2026/02/01 16:31:28 by aoqdeh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int     main(int argc, char **argv)
 {
     t_mlx   mlx;
     
-    init_vals(mlx);
-    decide_fract(argc, argv, mlx);
+    init_vals(&mlx);
+    decide_fract(argc, argv, &mlx);
     mlx.obj = mlx_init();
     if (!mlx.obj)
         return (-1);
@@ -29,7 +29,7 @@ int     main(int argc, char **argv)
     if (!mlx.img.image)
         close_mlx(&mlx);
     mlx.img.addr = mlx_get_data_addr(mlx.img.image, &mlx.img.bpp, &mlx.img.ll, &mlx.img.endian);
-    print_fractol(mlx);
+    print_fractol(&mlx);
     mlx_key_hook(mlx.win, handle_esc, &mlx);
     mlx_mouse_hook(mlx.win, handle_esc, &mlx);
     mlx_hook(mlx.win, 17, 0, close_mlx, &mlx);
